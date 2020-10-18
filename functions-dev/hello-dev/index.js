7dd0083912d3aabc741a3ea16de9fc474df71c0e
Derek Gavey <dgavey@gavey.ca>
2020-10-18 11:49:30 -0600

    Initial Commit

---
+++ functions-dev/hello-dev/index.js
@@ -0,0 +1,13 @@
+// Require the framework and instantiate it
+const api = require('lambda-api')()
+
+// Define a route
+api.get('/hello', async (req,res) => {
+    return { text: 'Hello Dev.to from Lambda API' }
+  })
+
+// Declare your Lambda handler
+exports.handler = async (event, context) => {
+  // Run the request
+  return await api.run(event, context)
+}
